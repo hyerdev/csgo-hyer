b13864d23d267f6b96b83a3fd01b8e1587b08af6
LucasRMP <lucas.rmagalhaes@gmail.com>
2020-04-08 19:54:34 -0300

    fix: remove index.css

---
+++ src/pages/CreateCfg/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '~/components/Layout';
+
+function CreateCfg() {
+  return (
+    <Layout>
+      <div>
+        <h1>CreateCfg</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default CreateCfg;
