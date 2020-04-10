58f34f2c379a4a285f443b9eb0193ed0ae34f840
Arthur Castro <arthurcastroes@gmail.com>
2020-04-10 16:52:19 -0300

    feat(global): add binds page
    add the page for the user to see the binds they`ve created using the create-cfg page

---
+++ src/pages/Binds/styles.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: fit-content;
+  padding-top: 150px;
+  background-color: #273366;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+
+  h1 {
+    font-size: 60px;
+    margin-bottom: 5px;
+  }
+
+  p {
+    font-size: 34px;
+    max-width: 320px;
+    color: #e6e6e6;
+  }
+`;
+
+export const BindsPanel = styled.div`
+  margin-top: 10px;
+
+  textarea {
+    background-color: #44549c;
+    border: none;
+    color: #e6e6e6;
+    outline: none;
+    resize: none;
+  }
+`;
+
+export const CFGPreview = styled.div``;
+export const Tutorial = styled.div`
+  text-align: right;
+  height: auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const CallToAction = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-start;
+
+  h2 {
+    margin-bottom: 5px;
+  }
+  p {
+    max-width: none;
+    font-size: 18px;
+    margin-bottom: 15px;
+  }
+
+  a {
+    display: block;
+    background: #c83232;
+    width: fit-content;
+
+    padding: 10px 20px;
+
+    border-radius: 3px;
+
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 30px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+`;
